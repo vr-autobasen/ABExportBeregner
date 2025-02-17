6686c84369e4b385619c85a6b24ad3d08ef147e6
vr-autobasen <vr@autobasen.dk>
2025-02-17 14:00:10 +0100

    Hvis export_tax er større end en reg tax - return regtax.

--- ExportCalc_inkl_van.py
+++ ExportCalc_inkl_van.py
@@ -22,7 +22,7 @@ def load_config():
 def update_from_github():
     try:
         import requests
-        raw_url = "https://raw.githubusercontent.com/vr-autobasen/ABExportBeregner/refs/heads/main/ExportCalc_inkl_van.py?token=GHSAT0AAAAAAC6YCSJMEYA245BBNOFQJRIWZ5TFRVQ"
+        raw_url = "https://raw.githubusercontent.com/vr-autobasen/ABExportBeregner/refs/heads/main/ExportCalc_inkl_van.py"
         response = requests.get(raw_url)
 
         if response.status_code == 200:
